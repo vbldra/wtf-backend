103e68648ce8331268c3cad4cddb9f8a38c890b7
Mamuna Anwar <mamuna_anwar@hotmail.com>
2021-06-28 09:43:06 +0200

    starting with testing

--- dataSources/middlePoint.js
+++ dataSources/middlePoint.js
@@ -1,9 +1,9 @@
 const { default: axios } = require("axios");
 
 const { getCenterOfBounds } = require("geolib");
-const { getCenter } = require("geolib");
 require("dotenv").config();
 const keyAPI = process.env.API_KEY;
+
 const getCoordinates = async (peopleAddresses) => {
   const geoPeopleAddresses = []; // array for co-ordinates
   try {
@@ -24,16 +24,8 @@ const getCoordinates = async (peopleAddresses) => {
     console.log({ geoPeopleAddresses });
     const geoMiddle = getCenterOfBounds(geoPeopleAddresses);
 
-    //const geoMiddle = getCenter(geoPeopleAddresses);
-    // const middleAddress = await axios.get(
-    //   `https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=${keyAPI}&pos=${geoMiddle.latitude},${geoMiddle.longitude},0&mode=retrieveAll&prox=${geoMiddle.latitude},${geoMiddle.longitude},5`
-    // );
-    // console.log(middleAddress.data.Response.View[0].Result[0].Location);
     console.log(geoMiddle);
     return geoMiddle;
-    //return middleAddress.data.Response.View[0].Result[0].Location
-    //.DisplayPosition;
-    //return middleAddress.data.Response.View[0].Result[0].Location; // Saving the label (city, country and so on)
   } catch (error) {
     throw new Error(error);
   }
