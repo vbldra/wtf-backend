9c164b45375cfafb696f8b8734e10c28ae4a5ff6
Mamuna <mamuna_anwar@hotmail.com>
2021-07-13 13:45:25 +0200

    hotels, restaurants, midpoint addresses sent to frontend

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 const router = Router();
 const {
   getLocationData,
-  getClosestCity,
+  getHotelsInformation,
+  getRestaurantsInformation,
 } = require("../controllers/apiRequestController");
 
 router.post("/", getLocationData);
-//router.post("/city", getClosestCity);
+router.post("/hotels", getHotelsInformation);
+router.post("/restaurants", getRestaurantsInformation);
 module.exports = router;
